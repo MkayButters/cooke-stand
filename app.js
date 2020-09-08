c2c12cfe68696390a90a52f69d159ef6001b1c39
RYAN PILON <Ryanpilon1995@gmail.com>
2020-09-07 17:55:16 -0700

    added more js

--- app.js
+++ app.js
@@ -1,21 +1,111 @@
 'use strict'
 
-seattle = {
+var storeinfo = document.getElementById('storeinfo')
+
+function customersPerHrRandom(min,max) {
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+var seattle = {
 
     store1: 'seattle',
-    minCustomers1: 23,
-    maxCustomers1: 65,
-    avgcookies1: 6.3,
-    times1: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '],
+    minCustomers: 23,
+    maxCustomers: 65,
+    avgcookies: 6.3,
+    times: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '],
 
-    function(customersPerHrRandom1) {
-        return Math.floor(Math.random() * (10 + 1));
+    render: function () {
+        for (var i = 0; i <= this.times.length; i++) {
+            var liEl = document.createElement('li')
+            liEl.textContent = this.times[i] + Math.round(customersPerHrRandom(this.minCustomers , this.maxCustomers)*this.avgcookies);
+            storeinfo.appendChild(liEl)
+            
+        }
     }
-
-    function(display) {
-        document.createElement((times1[0] + 1) + customersPerHrRandom1);
-    }
-    
 }
-console.log(customersPerHrRandom1);
+seattle.render()
+console.log(customersPerHrRandom)
+
+var tokyo = {
+
+    store: 'tokyo',
+    minCustomers: 3,
+    maxCustomers: 24,
+    avgcookies: 1.2,
+    times: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '],
+
+    render: function () {
+        for (var i = 0; i <= this.times.length; i++) {
+            var liEl = document.createElement('li')
+            liEl.textContent = this.times[i] + Math.round(customersPerHrRandom(this.minCustomers , this.maxCustomers)*this.avgcookies);
+            storeinfo.appendChild(liEl)
+            
+        }
+    }
+}
+tokyo.render()
+console.log(customersPerHrRandom)
+
+var dubai = {
+
+    store: 'dubai',
+    minCustomers: 11,
+    maxCustomers: 38,
+    avgcookies: 3.7,
+    times: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '],
+
+    render: function () {
+        for (var i = 0; i <= this.times.length; i++) {
+            var liEl = document.createElement('li')
+            liEl.textContent = this.times[i] + Math.round(customersPerHrRandom(this.minCustomers , this.maxCustomers)*this.avgcookies);
+            storeinfo.appendChild(liEl)
+            
+        }
+    }
+}
+dubai.render()
+console.log(customersPerHrRandom)
+
+var paris = {
+
+    store: 'paris',
+    minCustomers: 20,
+    maxCustomers: 38,
+    avgcookies: 2.3,
+    times: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '],
+
+    render: function () {
+        for (var i = 0; i <= this.times.length; i++) {
+            var liEl = document.createElement('li')
+            liEl.textContent = this.times[i] + Math.round(customersPerHrRandom(this.minCustomers , this.maxCustomers)*this.avgcookies);
+            storeinfo.appendChild(liEl)
+            
+        }
+    }
+}
+paris.render()
+console.log(customersPerHrRandom)
+
+var lima = {
+
+    store: 'lima',
+    minCustomers: 2,
+    maxCustomers: 16,
+    avgcookies: 4.6,
+    times: ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: '],
+
+    render: function () {
+        for (var i = 0; i <= this.times.length; i++) {
+            var liEl = document.createElement('li')
+            liEl.textContent = this.times[i] + Math.round(customersPerHrRandom(this.minCustomers , this.maxCustomers)*this.avgcookies);
+            storeinfo.appendChild(liEl)
+            
+        }
+    }
+}
+lima.render()
+console.log(customersPerHrRandom)
 
